847586501465be9a9ff12a5eed28357086bb0e8f
ZhenHangZhang <zhanghangzhen@ezpdemac-mini-2.local>
2016-03-31 17:36:32 +0800

    Initial Commit

---
+++ choutishitu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  choutishitu
+//
+//  Created by zhanghangzhen on 16/3/31.
+//  Copyright © 2016年 zhanghangzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ choutishitu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  choutishitu
+//
+//  Created by zhanghangzhen on 16/3/31.
+//  Copyright © 2016年 zhanghangzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
